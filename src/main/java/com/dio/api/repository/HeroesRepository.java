c913a13d31c93141ea726c888a01e9a9eb7aaaa5
oddmayer <gomtom@outlook.com>
2021-11-21 15:34:22 -0300

    main

---
+++ src/main/java/com/dio/api/repository/HeroesRepository.java
@@ -0,0 +1,10 @@
+package com.dio.api.repository;
+
+import com.dio.api.document.Heroes;
+
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+@EnableScan
+public interface HeroesRepository extends CrudRepository<Heroes, String>{
+}
